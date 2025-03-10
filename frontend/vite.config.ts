9461ea0d9646ce7c8882f278bd840a960ac758df
hayyaanp <95776319+hayyaanp@users.noreply.github.com>
2025-03-09 23:38:29 -0400

    configure shadcn/ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vite.dev/config
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
+  plugins: [react(), tailwindcss()],
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173,
+  // },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-});
+})
+
